5f85dc41e25b40ef421a5c840ec09b628c1110b8
kaizhang <zk65900931@gmail.com>
2014-04-01 17:10:40 -0700

    better version

--- chipseq/vector.hs
+++ chipseq/vector.hs
@@ -5,10 +5,13 @@ import qualified Data.ByteString.Lazy.Char8 as B
 import qualified Data.Vector.Generic as G
 import qualified Data.Vector.Unboxed as V
 import qualified Data.Vector.Generic.Mutable as GM
+import qualified Data.HashSet as S
 import Control.Monad
 import Data.Maybe
 import Data.List
 import System.Environment
+import Graphics.Rendering.HPlot
+import Data.Default
 
 toInt ∷ B.ByteString → Int
 toInt = fst . fromJust . B.readInt
@@ -22,22 +25,15 @@ readBed = foldl' go ([],[])
                                   "-" → (forwd, toInt end : rev)
                                   _ → error "read BED fail"
 
+
 readInterval ∷ [B.ByteString] → [(Int, Int)]
 readInterval = map f 
     where
         f x = let (chr:start:end:m:_) = B.words x
               in (toInt start, toInt end)
 
-storeTags ∷ G.Vector v Bool ⇒ [Int] → Int → v Bool
-storeTags tags chrSize = G.create (GM.replicate chrSize False >>= go tags)
-    where
-        go (x:xs) v = do
-            GM.write v x True
-            go xs v
-        go _ v = return v
-
 mkMap ∷ G.Vector v Bool ⇒ [(Int, Int)] → Int → v Bool
-mkMap intervals chrSize = G.create (GM.replicate chrSize False >>= go intervals)
+mkMap intervals chrSize = G.create (GM.replicate (chrSize+1) False >>= go intervals)
     where
         go (x:xs) v = do
             forM_ [fst x .. snd x] (\i → 
@@ -45,41 +41,52 @@ mkMap intervals chrSize = G.create (GM.replicate chrSize False >>= go intervals)
             go xs v
         go _ v = return v
 
-mkDMap ∷ G.Vector v Bool ⇒ v Bool → Int → Int → v Bool
-mkDMap m d readlength = G.create (GM.replicate (G.length m) False >>= go)
+-- | Is given position mappable at reverse strand shifted by d
+mappableR ∷ G.Vector v Bool ⇒ v Bool → Int → Int → Int → Bool
+mappableR m readlength i d = let shiftedPos = i + d - readlength + 1
+                                 l = G.length m
+                               in if shiftedPos >= 0 && shiftedPos < l
+                                     then G.unsafeIndex m shiftedPos
+                                     else False
+
+doubleMappable ∷ G.Vector v Bool ⇒ v Bool → Int → Int → Int → Bool
+doubleMappable m readlength i d | i < G.length m = G.unsafeIndex m i && mappableR m readlength i d
+                                | otherwise = False
+
+-- | number of double mappable regions given d
+numOfDM ∷ (Num a, G.Vector v Bool) ⇒ v Bool → Int → Int → a
+numOfDM m readlength d = foldl' f 0 [0..l]
     where
-        go v = do
-            forM_ [0..G.length m - 1] (\i → do
-                let shift = i + d -readlength + 1
-                when ((shift >= 0) && G.unsafeIndex m i && G.unsafeIndex m shift) $
-                    GM.write v i True)
-            return v
+        l = G.length m
+        f acc i | doubleMappable m readlength i d = acc + 1
+                | otherwise = acc
 
-intersection ∷ G.Vector v Bool ⇒ v Bool → v Bool → v Bool
-intersection a b = G.zipWith (&&) a b
-
-numOfOnes ∷ (Num a, G.Vector v Bool) ⇒ v Bool → a
-numOfOnes = fromIntegral . G.length . G.filter id
-
-corr ∷ G.Vector v Bool ⇒ v Bool → v Bool → v Bool → Double
-corr forwd rev m = (count / n - μ1 * μ2) / sqrt (v1 * v2)
+countReadsDM ∷ (Num a, G.Vector v Bool) ⇒ S.HashSet Int → v Bool → Int → Int → a
+countReadsDM rs m readlength d = S.foldl' f 0 rs
     where
-        n = numOfOnes m
-        forwd' = intersection forwd m
-        rev' = intersection rev m
-        μ1 = numOfOnes forwd' / n
-        μ2 = numOfOnes rev' / n
+        f acc i | doubleMappable m readlength i d = acc + 1
+                | otherwise = acc 
+
+count_ ∷ (Num a, G.Vector v Bool) ⇒ S.HashSet Int → S.HashSet Int → v Bool → Int → Int → a
+count_ forwd rev m readlength d = S.foldl' f 0 rev
+    where
+        f acc i = let i' = i - d
+                  in if (i' `S.member` forwd && doubleMappable m readlength i' d)
+                        then acc + 1
+                        else acc
+    
+
+corrAtD ∷ G.Vector v Bool ⇒ S.HashSet Int → S.HashSet Int → v Bool → Int → Int → Double
+corrAtD forwd rev m readlength d = (count / n - μ1 * μ2) / sqrt (v1 * v2)
+    where
+        n = numOfDM m readlength d
+        μ1 = countReadsDM forwd m readlength d / n
+        μ2 = countReadsDM rev m readlength d / n
         v1 = μ1 * (1 - μ1)
         v2 = μ2 * (1 - μ2)
-        count = numOfOnes $ intersection forwd' rev'
+        count = count_ forwd rev m readlength d
 
-corrAtD ∷ G.Vector v Bool ⇒ v Bool → v Bool → v Bool → Int → Int → Double
-corrAtD forwd rev m readlength d = result `seq` result
-    where
-        result = corr forwd rev dm
-        dm = mkDMap m d readlength
-
-corrOverRange ∷ G.Vector v Bool ⇒ v Bool → v Bool → v Bool → Int → [Int] → [Double]
+corrOverRange ∷ G.Vector v Bool ⇒ S.HashSet Int → S.HashSet Int → v Bool → Int → [Int] → [Double]
 corrOverRange forwd rev m readlength = map (corrAtD forwd rev m readlength)
 
 main ∷ IO ()
@@ -90,9 +97,11 @@ main = do
     let (forwd, rev) = readBed $ B.lines reads
         intervals = readInterval $ tail $ B.lines maps
         chrSize = 166650296
-        forwd' = storeTags forwd chrSize ∷ V.Vector Bool
-        rev' = storeTags rev chrSize ∷ V.Vector Bool
+        forwd' = S.fromList forwd
+        rev' = S.fromList rev
         m = mkMap intervals chrSize ∷ V.Vector Bool
         readlength = 36
-        drange = [1..1]
-    print $ corrOverRange forwd' rev' m readlength drange
+        x = [1,3..200]
+        y = corrOverRange forwd' rev' m readlength x
+    plot' def [line def (Just $ map fromIntegral x, y)] "2.png"
+    return ()
