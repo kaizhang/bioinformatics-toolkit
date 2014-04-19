67e1578683b306b84bd8356116f6a00502aed9c3
kaizhang <zk65900931@gmail.com>
2014-04-18 21:44:57 -0700

    finish naive cross correlation estimator

--- Bio/ChIPSeq/ChIP.hs
+++ Bio/ChIPSeq/ChIP.hs
@@ -2,32 +2,38 @@
 {-# LANGUAGE FlexibleContexts #-}
 
 module ChIP (
-      apprxCorr
+      naiveCC
     , corrOverRange
-    , readBed
     , slidingAverage
     , readInterval
     , binarySearch
 ) where
 
-import qualified Data.ByteString.Lazy.Char8 as B
+import qualified Data.ByteString.Char8 as B
 import qualified Data.Vector.Generic as G
 import qualified Data.Vector.Unboxed as V
 import qualified Data.HashSet as S
+import qualified Data.HashMap.Strict as M
 import Data.Maybe
 import Data.Bits
-import Data.List
 import Control.Concatenative
 import Control.Parallel.Strategies
 import Statistics.Sample (mean)
 
 -- | fast relative cross-correlation
 apprxCorr ∷ S.HashSet Int → S.HashSet Int → Int → Int → Double
+{-# INLINE apprxCorr #-}
 apprxCorr forwd rev smooth d = fromIntegral $ S.foldl' f 0 rev
     where
         f ∷ Int → Int → Int
         f !acc r | any (`S.member` forwd) [r-d-smooth..r-d+smooth] = acc + 1
                  | otherwise = acc
+
+naiveCC ∷ B.ByteString → [Int] → Double
+naiveCC input range = mean.V.fromList.map (maxCC.snd) $ chrs
+    where
+        chrs = readBed input
+        maxCC (forwd, rev) = fromIntegral.snd.maximum $ zip (parMap rpar (apprxCorr forwd rev 3) range) range
 
 type Interval = (Int, Int)
 
@@ -89,16 +95,21 @@ binarySearch t v = binarySearchWithBound compare_ t v 0 $ G.length v
 
 
 toInt ∷ B.ByteString → Int
+{-# INLINE toInt #-}
 toInt = fst . fromJust . B.readInt
 
-readBed ∷ [B.ByteString] → ([Int], [Int])
-readBed = foldl' go ([],[])
+readBed ∷ B.ByteString → [(B.ByteString, (S.HashSet Int, S.HashSet Int))]
+{-# INLINE readBed #-}
+readBed = map toSet.M.toList.M.fromListWith f.map parseLine.B.lines
     where
-        go (!forwd, !rev) x = let (_:start:end:_:_:strand:_) = B.words x
-                              in case strand of
-                                  "+" → (toInt start : forwd, rev)
-                                  "-" → (forwd, toInt end : rev)
-                                  _ → error "read BED fail"
+        parseLine ∷ B.ByteString → (B.ByteString, ([Int], [Int]))
+        parseLine x = let (chr:start:end:_:_:strand:_) = B.words x
+                      in case strand of
+                          "+" → (chr, ([toInt start], []))
+                          "-" → (chr, ([], [toInt end]))
+                          _ → error "Unknown Strand!"
+        f (a,b) (a',b') = (a++a', b++b')
+        toSet (chr, (forwd, rev)) = (chr, (S.fromList forwd, S.fromList rev))
 
 
 readInterval ∷ [B.ByteString] → [Interval]
--- Bio/ChIPSeq/fragment_length.hs
+++ Bio/ChIPSeq/fragment_length.hs
@@ -1,43 +1,14 @@
 {-# LANGUAGE OverloadedStrings, UnicodeSyntax #-}
 
-import qualified Data.ByteString.Lazy.Char8 as B
+import qualified Data.ByteString.Char8 as B
 import System.Environment
-import Graphics.Rendering.HPlot
-import Data.Default
-import Control.Parallel.Strategies
-import qualified Data.HashSet as S
 import ChIP
-import Control.Lens hiding (both)
-import Control.Concatenative
-import Spline
 
 run ∷ [FilePath] → IO ()
-run [f1,f2,f3] = do
+run [f1] = do
     tags ← B.readFile f1
-    maps ← B.readFile f2
-    let (forwd, rev) = both S.fromList $ readBed $ B.lines tags
-        m = readInterval $ tail $ B.lines maps
-        x = [0,1..400]
-        y = corrOverRange forwd rev m (read f3) x
-        y' = slidingAverage y 15
-    _ ← plot' def [
-        line def (Just $ map fromIntegral x, y)
---        line (opacity .~ 0.7 $ col .~ "green" $ def) (Just $ map fromIntegral x, y')
-        ] "1.png"
-    return ()
-
-run [f1,f2] = do
-    tags ← B.readFile f1
-    let (forwd, rev) = both S.fromList $ readBed $ B.lines tags
-        x = [0,1..400]
-        y = parMap rseq (apprxCorr forwd rev (read f2)) x
-    (x',y') ← smoothSpline (zip (map fromIntegral x) y) 0.6
-    _ ← plot' def [
-        line def (Just $ map fromIntegral x, y)
---        line (col .~ "black" $ def) (Just x', y')
-        ] "1.png"
-    return ()
-
+    let x = [55..400]
+    print $ naiveCC tags x
 run _ = error "incorrect arguments"
 
 main ∷ IO ()
