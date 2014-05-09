969dd90237ed052a2e1684fb83a69e58e0df0ecb
kaizhang <zk65900931@gmail.com>
2014-05-09 01:05:07 -0700

    use state monad to parse bed

--- Bio/Utils/Bed.hs
+++ Bio/Utils/Bed.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE DeriveGeneric #-}
 
 module Bio.Utils.Bed (
       readInt
@@ -22,6 +23,9 @@ import qualified Data.ByteString.Lex.Double as L
 import qualified Data.ByteString.Lex.Lazy.Double as LL
 import Data.Maybe
 import Control.Lens
+import Control.Monad.State.Strict
+import Data.Default.Generics
+import GHC.Generics
 
 -- | the type for BED format, as described in http://genome.ucsc.edu/FAQ/FAQformat.html#format1.7
 data BED = BED
@@ -31,9 +35,11 @@ data BED = BED
     , _name :: Maybe BL.ByteString
     , _score :: Maybe Double
     , _strand :: Maybe Bool
-    } deriving (Show)
+    } deriving (Show, Generic)
 
 makeLenses ''BED
+
+instance Default BED
 
 readBED :: FilePath -> IO [BED]
 readBED fl = do content <- BL.readFile fl
@@ -41,16 +47,29 @@ readBED fl = do content <- BL.readFile fl
 
 fromLine :: BL.ByteString -> BED
 {-# INLINE fromLine #-}
-fromLine l = case BL.words l of
-    [chr,s,e] -> BED chr (readInt s) (readInt e) Nothing Nothing Nothing
-    [chr,s,e,nm,sc] -> BED chr (readInt s) (readInt e) (getName nm) (getScore sc) Nothing
-    [chr,s,e,nm,sc,str] -> BED chr (readInt s) (readInt e) (getName nm) (getScore sc) (getStrand str)
-    _ -> error "Read BED fail: Incorrect number of fields"
+fromLine l = evalState (f (BL.split '\t' l)) 1
   where
-    getName nm | nm == "." = Nothing
-               | otherwise = Just nm
-    getScore s | s == "." = Nothing
-               | otherwise = Just . readDouble $ s
+    f :: [BL.ByteString] -> State Int BED
+    f [] = do i <- get
+              if i <= 3 then error "Read BED fail: Incorrect number of fields"
+                        else return def
+    f (x:xs) = do 
+        i <- get
+        put (i+1)
+        bed <- f xs
+        case i of
+            1 -> return $ chrom .~ x $ bed
+            2 -> return $ chromStart .~ readInt x $ bed
+            3 -> return $ chromEnd .~ readInt x $ bed
+            4 -> return $ name .~ guard' x $ bed
+            5 -> return $ score .~ getScore x $ bed
+            6 -> return $ strand .~ getStrand x $ bed
+            _ -> return def
+
+    guard' x | x == "." = Nothing
+             | otherwise = Just x
+    getScore x | x == "." = Nothing
+               | otherwise = Just . readDouble $ x
     getStrand str | str == "-" = Just False
                   | str == "+" = Just True
                   | otherwise = Nothing
@@ -61,11 +80,15 @@ class ToNum a where
 
 instance ToNum B.ByteString where
     readInt = fst.fromJust.B.readInt
+    {-# INLINE readInt #-}
     readDouble = fst . fromJust . L.readDouble
+    {-# INLINE readDouble #-}
 
 instance ToNum BL.ByteString where
     readInt = fst.fromJust.BL.readInt
+    {-# INLINE readInt #-}
     readDouble = fst . fromJust . LL.readDouble
+    {-# INLINE readDouble #-}
 
 -- | divide a given region into fixed size fragments
 binBySize :: (Int, Int) -> Int -> [(Int, Int)]
