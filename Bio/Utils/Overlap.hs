3d4969588c42ddbb251be4111b453e73ddefb831
kaizhang <zk65900931@gmail.com>
2014-05-20 13:57:07 -0700

    use conduit for streamming, remove Lazy IO

--- Bio/Utils/Overlap.hs
+++ Bio/Utils/Overlap.hs
@@ -15,6 +15,9 @@ import Data.Function
 import Bio.Utils.Bed
 import Control.Monad
 import Control.Lens ((^.))
+import Data.Conduit
+import qualified Data.Conduit.List as CL
+import Control.Monad.Trans.Class (lift)
 
 -- | convert lines of a BED file into a data structure - A hashmap of which the
 -- | chromosomes, and values are interval maps.
@@ -25,17 +28,22 @@ toMap input = M.fromList.map create.groupBy ((==) `on` (fst.fst)) $ zip input [0
         f ((_, x), i) = (toInterval x, i)
         create xs = (fst.fst.head $ xs, IM.fromDistinctAscList.map f $ xs)
 
+{-
 -- | coverages of bins
--- FIXME: space leak
+-- FIXME: Too ugly
 coverage :: [BED]  -- ^ genomic locus in BED format
          -> [BED]  -- ^ reads in BED format
-         -> V.Vector Double
-coverage bin tags = V.zipWith normalize (V.create (VM.replicate n 0 >>= go tags)) featWidth
+         -> (V.Vector Double, Int)
+coverage bin tags = getResult (V.create (VM.replicate (n+1) 0 >>= go tags))
   where
+    getResult v = (V.zipWith normalize (V.slice 0 n v) featWidth, v V.! n)
     go ts v = do
         forM_ ts (\t -> do
             let set = M.lookup (t^.chrom) featMap
-                b = (t^.chromStart, t^.chromEnd)
+                s = t^.chromStart
+                e = t^.chromEnd
+                b = (s, e)
+                l = s - e + 1
                 intervals = case set of
                     Just iMap -> IM.intersecting iMap.toInterval $ b
                     _ -> []
@@ -44,8 +52,44 @@ coverage bin tags = V.zipWith normalize (V.create (VM.replicate n 0 >>= go tags)
                     nucl = overlap b . fst $ interval
                 VM.write v i . (+nucl) =<< VM.read v i
                 )
+            VM.write v n . (+l) =<< VM.read v n
             )
         return v
+    featMap = toMap.map (\x -> (x^.chrom, (x^.chromStart, x^.chromEnd))) $ bin
+    featWidth = V.fromList.map (\x -> x^.chromEnd - x^.chromStart) $ bin
+    n = length bin
+    overlap (l, u) (IM.ClosedInterval l' u') 
+        | l' >= l = if u' <= u then u'-l'+1 else u-l'+1
+        | otherwise = if u' <= u then u'-l+1 else u-l+1
+    overlap _ _ = 0
+    normalize a b = fromIntegral a / fromIntegral b
+    -}
+
+coverage :: [BED]  -- ^ genomic locus in BED format
+         -> Source IO BED  -- ^ reads in BED format
+         -> IO (V.Vector Double, Int)
+coverage bin tags = liftM getResult $ tags $$ sink
+  where
+    sink :: Sink BED IO (V.Vector Int)
+    sink = do
+        v <- lift $ VM.replicate (n+1) 0
+        CL.mapM_ $ \t -> do
+                let set = M.lookup (t^.chrom) featMap
+                    s = t^.chromStart
+                    e = t^.chromEnd
+                    b = (s, e)
+                    l = e - s + 1
+                    intervals = case set of
+                        Just iMap -> IM.intersecting iMap.toInterval $ b
+                        _ -> []
+                forM_ intervals (\interval -> do 
+                    let i = snd interval
+                        nucl = overlap b . fst $ interval
+                    VM.write v i . (+nucl) =<< VM.read v i
+                    )
+                VM.write v n . (+l) =<< VM.read v n
+        lift $ V.freeze v
+    getResult v = (V.zipWith normalize (V.slice 0 n v) featWidth, v V.! n)
     featMap = toMap.map (\x -> (x^.chrom, (x^.chromStart, x^.chromEnd))) $ bin
     featWidth = V.fromList.map (\x -> x^.chromEnd - x^.chromStart) $ bin
     n = length bin
