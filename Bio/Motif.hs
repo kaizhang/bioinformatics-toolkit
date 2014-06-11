a1f0c9a10d838f3f84134c91c07540ad4268a057
kaizhang <zk65900931@gmail.com>
2014-06-10 23:25:54 -0700

    work on Bio.Motif

--- Bio/Motif.hs
+++ Bio/Motif.hs
@@ -1,20 +1,41 @@
-module Bio.Motif where
+module Bio.Motif 
+    ( score
+    , readPWM
+    , scores
+    ) where
 
-import Data.Array.Unboxed
 import qualified Data.ByteString.Char8 as B
 import Data.List
+import Data.Default.Generics
+import Bio.Utils.Bed (readDouble)
+import Data.Matrix
 
--- | position weight matrix for motifs
-newtype PWM = PWM (UArray (Int, Int) Double)
+-- | k x 4 position weight matrix for motifs
+newtype PWM = PWM (Matrix Double)
 
-score :: PWM -> B.ByteString -> Double
-score (PWM pwm) sequ = foldl' (+) 0 . map f $ [0 .. len-1]
+newtype BkgdModel = BG (Double, Double, Double, Double)
+
+instance Default BkgdModel where
+    def = BG (0.25, 0.25, 0.25, 0.25)
+
+-- | get scores of a long sequences at each position
+scores :: BkgdModel -> PWM -> B.ByteString -> [Double]
+scores bg p@(PWM pwm) = go
+  where
+    go s | B.length s >= len = score bg p (B.take len s) : go (B.tail s)
+         | otherwise = []
+    len = nrows pwm
+
+score :: BkgdModel -> PWM -> B.ByteString -> Double
+score (BG (a, c, g, t)) (PWM pwm) sequ = foldl' (+) 0 . map f $ [0 .. len-1]
   where
     f i = case sequ `B.index` i of
-              'A' -> logBase 10 (pwm ! (0, i))
-              'C' -> logBase 10 (pwm ! (1, i))
-              'G' -> logBase 10 (pwm ! (2, i))
-              'T' -> logBase 10 (pwm ! (3, i))
+              'A' -> log $ (pwm ! (i, 0)) / a
+              'C' -> log $ (pwm ! (i, 1)) / c
+              'G' -> log $ (pwm ! (i, 2)) / g
+              'T' -> log $ (pwm ! (i, 3)) / t
               _   -> error "Bio.Motif.score: invalid nucleotide!"
-    len = snd.snd.bounds $ pwm
+    len = nrows pwm
 
+readPWM :: B.ByteString -> PWM
+readPWM x = PWM $! fromLists . map (map readDouble.B.words) . B.lines $ x
