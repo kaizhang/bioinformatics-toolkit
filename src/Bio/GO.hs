99ff684358b26977de9e8ad8d6dbd1f9de9d1d60
Kai Zhang <kai@kzhang.org>
2015-02-10 16:49:21 -0800

    minor

--- src/Bio/GO.hs
+++ src/Bio/GO.hs
@@ -2,22 +2,33 @@
 module Bio.GO
     ( GO(..)
     , GOId
+    , GOTree
+    , getParent
     ) where
 
 import qualified Data.Text as T
 import Data.Text.Encoding (decodeUtf8)
 import qualified Data.ByteString.Char8 as B
+import qualified Data.HashMap.Strict as M
 import Data.Maybe (fromMaybe)
 
-type GOId = B.ByteString
-
 data GO = GO
-    { _label :: !T.Text
-    , _oboId :: !GOId
-    , _oboNS :: !T.Text
+    { _oboId :: !GOId
+    , _label :: !T.Text
     , _subProcessOf :: !(Maybe GOId)
+    , _oboNS :: !T.Text
     }
 
 instance Show GO where
     show (GO a b c d) = T.unpack $ T.intercalate "\t"
-        [a, decodeUtf8 b, c, decodeUtf8 $ fromMaybe "" d]
+        [decodeUtf8 a, b, decodeUtf8 $ fromMaybe "" c, d]
+
+type GOId = B.ByteString
+
+type GOTree = M.HashMap GOId GO
+
+getParent :: GO -> GOTree -> Maybe GO
+getParent g tree = case _subProcessOf g of
+    Just i -> M.lookup i tree
+    _ -> Nothing
+{-# INLINE getParent #-}
--- src/Bio/GO/Parser.hs
+++ src/Bio/GO/Parser.hs
@@ -5,6 +5,7 @@ module Bio.GO.Parser
 
 import Control.Applicative ((<$>))
 import qualified Data.ByteString.Lazy.Char8 as L
+import qualified Data.HashMap.Strict as M
 import Data.Maybe
 import qualified Data.Text as T
 import Data.Text.Encoding (encodeUtf8)
@@ -13,14 +14,19 @@ import Text.XML.Expat.Tree
 
 import Bio.GO
 
-readOWL :: FilePath -> IO [GO]
-readOWL fl = do c <- L.readFile fl
-                let (xml, _) = parse defaultParseOptions c :: (Node T.Text T.Text, Maybe XMLParseError)
-                    goTerms = findChildren "owl:Class" xml
-                return $ map pickle goTerms
+readOWL :: FilePath -> IO GOTree
+readOWL fl = do
+    c <- L.readFile fl
+    let (xml, _) = parse defaultParseOptions c
+        goTerms = findChildren "owl:Class" (xml :: Node T.Text T.Text)
+    return . M.fromList . map pickle $ goTerms
   where
-    pickle x = GO (f $ findChild "rdfs:label" x)
-                  (encodeUtf8 . f $ findChild "oboInOwl:id" x)
-                  (f $ findChild "oboInOwl:hasOBONamespace" x)
-                  ((encodeUtf8 . T.replace "_" ":" . snd . T.breakOnEnd "/" . snd . head . getAttributes) <$> findChild "rdfs:subClassOf" x)
+    pickle x =
+        let id' = encodeUtf8 . f $ findChild "oboInOwl:id" x
+            label = f $ findChild "rdfs:label" x
+            parent = ( encodeUtf8 . T.replace "_" ":" . snd
+                     . T.breakOnEnd "/" . snd . head . getAttributes
+                     ) <$> findChild "rdfs:subClassOf" x
+            namespace = f $ findChild "oboInOwl:hasOBONamespace" x
+        in (id', GO id' label parent namespace)
     f = getText . head . getChildren . fromJust
