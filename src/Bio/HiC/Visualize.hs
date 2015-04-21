d8247aac672cc50e7504aebdaef99b880efc48ee
Kai Zhang <kai@kzhang.org>
2015-04-21 16:43:12 -0700

    improve the performance of HiC visualization

--- src/Bio/HiC/Visualize.hs
+++ src/Bio/HiC/Visualize.hs
@@ -4,6 +4,7 @@ module Bio.HiC.Visualize
     , DrawHiCOpt(..)
     ) where
 
+import qualified Data.ByteString.Lazy as L
 import qualified Data.Matrix.Generic as MG
 import Data.Matrix.Symmetric (SymMatrix(..))
 import Codec.Picture
@@ -26,30 +27,57 @@ instance Default DrawHiCOpt where
     def = DrawHiCOpt
         { _range = Nothing
 --        , _colours = V.fromList $ brewerSet Reds 3
-        , _colours = V.fromList [white, red]
+        , _colours = V.fromList $ interpolate 62 white red
         }
 
 drawHiC :: FilePath -> ContactMap -> DrawHiCOpt -> IO ()
-drawHiC fl m opt = writePng fl $ matToImage (lo,hi) mat $ _colours opt
+drawHiC fl m opt = case encodePalettedPng pal pic of
+    Left e -> error e
+    Right png -> L.writeFile fl png
   where
+    pic = matToImage nCol (lo,hi) mat
+    pal = generateImage fn nCol 1
+      where
+        fn i _ = colorConvert $ cols V.! i
+        cols = _colours opt
+    nCol = V.length $ _colours opt
     mat@(SymMatrix _ v) = _matrix m
     (lo,hi) = case _range opt of
         Just (a,b) -> (a,b)
         _ -> (U.minimum v, U.maximum v)
 
-matToImage :: MG.Matrix m v Double => (Double, Double) -> m v Double -> V.Vector (Colour Double) -> Image PixelRGB8
-matToImage (lo,hi) mat cs = generateImage drawPixel r c
+matToImage :: MG.Matrix m v Double => Int -> (Double, Double) -> m v Double -> Image Pixel8
+matToImage n (lo,hi) mat = generateImage drawPixel r c
   where
-    drawPixel i j = pickColour $ mat `MG.unsafeIndex` (i,j)
+    drawPixel i j | x <= lo = 0
+                  | x >= hi = fromIntegral $ n - 1
+                  | otherwise = truncate $ (x - lo) / step
+      where
+        x = mat `MG.unsafeIndex` (i,j)
+    step = (hi - lo) / fromIntegral n
     (r,c) = MG.dim mat
-    pickColour v = colorConvert $ colorMapSmooth (lmap v) cs 
-    lmap = linearMapBound (lo,hi) (0,1)
 {-# INLINE matToImage #-}
 
 colorConvert :: Colour Double -> PixelRGB8
 colorConvert c = let RGB r g b = toSRGB24 c
                  in PixelRGB8 r g b
 {-# INLINE colorConvert #-}
+
+{-
+colorBlend :: Int -> V.Vector (Colour Double) -> V.Vector (Colour Double)
+colorBlend n colors | n <= m = colors
+                    | otherwise = 
+  where
+    m = V.length colors
+    -}
+
+interpolate :: Int -> Colour Double -> Colour Double -> [Colour Double]
+interpolate n c1 c2 = loop 1
+  where
+    loop i | i > n = []
+           | otherwise = blend (fromIntegral i * step) c2 c1 : loop (i+1)
+    step = 1 / fromIntegral (n+1)
+{-# INLINE interpolate #-}
 
 -- | map numbers to colors
 colorMapSmooth :: Double -- a value from 0 to 1
@@ -64,7 +92,7 @@ colorMapSmooth x colors = blend p (colors V.! i) $ colors V.! (i+1)
 
 linearMapBound :: (Double, Double) -> (Double, Double) -> Double -> Double
 linearMapBound (l, u) (l', u') x
-    | x < l = l' 
+    | isNaN x || x < l = l' 
     | x > u = u'
     | otherwise = (x - l) / (u - l) * (u' - l') + l'
 {-# INLINE linearMapBound #-}
