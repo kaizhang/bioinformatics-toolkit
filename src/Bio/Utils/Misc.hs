28e3baf3c5f884ce31e716d62e68828421e0488c
Kai Zhang <kai@kzhang.org>
2015-06-30 11:24:01 -0700

    use bytestring-lexing-0.5

--- src/Bio/Utils/Misc.hs
+++ src/Bio/Utils/Misc.hs
@@ -1,40 +1,29 @@
-module Bio.Utils.Misc (
-      readInt
+module Bio.Utils.Misc
+    ( readInt
     , readDouble
     , bins
     , binBySize
     , binBySizeLeft
-) where
+    ) where
 
-import qualified Data.ByteString.Char8 as B
-import qualified Data.ByteString.Lazy.Char8 as BL
-import qualified Data.ByteString.Lex.Double as L
-import qualified Data.ByteString.Lex.Lazy.Double as LL
-import Data.Maybe
+import Data.ByteString.Char8 (ByteString)
+import Data.ByteString.Lex.Fractional (readSigned, readExponential)
+import Data.ByteString.Lex.Integral (readDecimal)
+import Data.Maybe (fromMaybe)
 
-class ToNum a where
-    readInt :: a -> Int
-    readDouble :: a -> Double
+readInt :: ByteString -> Int
+readInt x = fst . fromMaybe errMsg . readSigned readDecimal $ x
+  where
+    errMsg = error $ "readInt: Fail to cast ByteString to Int:" ++ show x
+{-# INLINE readInt #-}
 
-instance ToNum B.ByteString where
-    readInt x = fst . fromMaybe raiseError . B.readInt $ x
-      where raiseError = error $ "Fail to cast ByteString to Int:" ++ show x
-    {-# INLINE readInt #-}
+readDouble :: ByteString -> Double
+readDouble x = fst . fromMaybe errMsg . readSigned readExponential $ x
+  where
+    errMsg = error $ "readDouble: Fail to cast ByteString to Double:" ++ show x
+{-# INLINE readDouble #-}
 
-    readDouble x = fst . fromMaybe raiseError. L.readDouble $ x
-      where raiseError = error $ "Fail to cast ByteString to Double:" ++ show x
-    {-# INLINE readDouble #-}
-
-instance ToNum BL.ByteString where
-    readInt x = fst. fromMaybe raiseError. BL.readInt $ x
-      where raiseError = error $ "Fail to cast ByteString to Int:" ++ show x
-    {-# INLINE readInt #-}
-
-    readDouble x = fst . fromMaybe raiseError . LL.readDouble $ x
-      where raiseError = error $ "Fail to cast ByteString to Double:" ++ show x
-    {-# INLINE readDouble #-}
-
--- | divide a given half-close-half-open region into fixed size 
+-- | divide a given half-close-half-open region into fixed size
 -- half-close-half-open intervals, discarding leftovers
 binBySize :: Int -> (Int, Int) -> [(Int, Int)]
 binBySize step (start, end) = let xs = [start, start + step .. end]
