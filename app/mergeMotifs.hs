caf4e8b96a217f01cbde94193b08896d4df9bcc0
Kai Zhang <kai@kzhang.org>
2016-03-17 10:03:29 -0700

    add dist command in mergeMotifs

--- app/mergeMotifs.hs
+++ app/mergeMotifs.hs
@@ -1,11 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import           AI.Clustering.Hierarchical hiding (drawDendrogram)
-import           Control.Monad              (forM)
-import qualified Data.ByteString.Char8      as B
+import           AI.Clustering.Hierarchical        hiding (drawDendrogram)
+import           Control.Monad                     (forM, forM_)
+import qualified Data.ByteString.Char8             as B
 import           Data.Default.Class
+import           Data.Double.Conversion.ByteString (toShortest)
 import           Data.List
-import           Data.List.Split            (splitOn)
+import           Data.List.Split                   (splitOn)
 import           Data.Ord
 {-
 import           Diagrams.Backend.Cairo
@@ -20,56 +21,51 @@ import           Bio.Data.Fasta
 import           Bio.Motif
 import           Bio.Motif.Alignment
 import           Bio.Motif.Merge
-import           Bio.Seq                    (toBS)
+import           Bio.Seq                           (toBS)
 import           Bio.Utils.Functions
 
-data Options = Options
-    { input    :: FilePath
-    , output   :: FilePath
-    , prefix   :: String
-    , thres    :: Double
-    , mode     :: String
-    , svg      :: Maybe FilePath
-    , dumpDist :: Bool
-    , gap      :: Double
-    , gapMode  :: String
-    , avgMode  :: String
-    } deriving (Show)
+data CMD = Merge { mergeInput  :: FilePath
+                 , mode        :: String
+                 , thres       :: Double
+                 , alignOption :: AlignOption
+                 , mergeOutput :: FilePath
+                 }
+         | Dist { inputA      :: FilePath
+                , inputB      :: Maybe FilePath
+                , alignOption :: AlignOption
+                }
 
-parser :: Parser Options
-parser = Options
-     <$> argument str (metavar "INPUT")
-     <*> strOption
-           ( long "output"
-          <> short 'o'
-          <> value "merged_output.meme"
-          <> metavar "OUTPUT" )
-     <*> strOption
-           ( long "prefix"
-          <> short 'p'
-          <> value "merged"
-          <> metavar "PREFIX"
-          <> help "PREFIX that being add to the name of the merged motif" )
-     <*> option auto
-           ( long "thres"
-          <> short 't'
-          <> value 0.2
-          <> metavar "THRESHOLD"
-          <> help "two motifs that have distance belowing threshold would be merged, default is 0.2" )
-     <*> strOption
-           ( long "mode"
-          <> short 'm'
-          <> value "iter"
-          <> metavar "MODE"
-          <> help "Merging algorithm, could be iter or tree, default is iter" )
-     <*> (optional . strOption)
-           ( long "svg"
-          <> metavar "SVG"
-          <> help "Draw merging tree in svg format, only available in tree mode" )
-     <*> switch
-           ( long "dump-dist"
-          <> help "Output pairwise distances of original motifs without performing any merging" )
-     <*> option auto
+mergeParser :: Parser CMD
+mergeParser = Merge
+    <$> argument str (metavar "INPUT")
+    <*> strOption
+        ( long "mode"
+       <> short 'm'
+       <> value "iter"
+       <> metavar "MODE"
+       <> help "Merging algorithm, could be iter or tree, default is iter" )
+    <*> option auto
+        ( long "thres"
+       <> short 't'
+       <> value 0.2
+       <> metavar "THRESHOLD"
+       <> help "two motifs that have distance belowing threshold would be merged, default is 0.2" )
+    <*> alignParser
+    <*> strOption
+        ( long "output"
+       <> short 'o'
+       <> value "merged_output.meme"
+       <> metavar "OUTPUT" )
+
+data AlignOption = AlignOption
+    { gap     :: Double
+    , gapMode :: String
+    , avgMode :: String
+    }
+
+alignParser :: Parser AlignOption
+alignParser = AlignOption
+     <$> option auto
            ( long "gap"
           <> short 'g'
           <> value 0.05
@@ -86,13 +82,19 @@ parser = Options
           <> metavar "AVERAGE_MODE"
           <> help "Averaging function, one of l1, l2, l3, max. default: l1." )
 
+distParser :: Parser CMD
+distParser = Dist
+    <$> strOption
+           ( short 'a'
+          <> metavar "INPUT_A" )
+    <*> (optional . strOption)
+           ( short 'b'
+          <> metavar "INPUT_B" )
+    <*> alignParser
 
-pairDistance :: [Motif] -> [(B.ByteString, B.ByteString, Double)]
-pairDistance ms = map (\(a,b) -> (_name a, _name b, fst $ alignment (_pwm a) (_pwm b))) $ comb ms
-
-treeMerge :: Double -> String -> [Motif] -> Double -> CombineFn
+treeMerge :: Double -> String -> [Motif] -> AlignOption
           -> ([Motif], Dendrogram Motif)
-treeMerge th pre ms gap combFn = (zipWith f [0::Int ..] $ map merge $ tree `cutAt` th, tree)
+treeMerge th pre ms alignOpt = (zipWith f [0::Int ..] $ map merge $ tree `cutAt` th, tree)
   where
     f i (suffix, pwm) = Motif ((B.pack $ pre ++ "_" ++ show i ++ "_" ++ show (toIUPAC pwm))
                                  `B.append` "("
@@ -101,55 +103,15 @@ treeMerge th pre ms gap combFn = (zipWith f [0::Int ..] $ map merge $ tree `cutA
     merge tr = ( B.intercalate "+" $ map _name $ flatten tr
                , dilute $ mergeTreeWeighted align tr)
     tree = buildTree align ms
-    align = alignmentBy jsd (quadPenal gap) combFn
+    align = mkAlignFn alignOpt
 {-# INLINE treeMerge #-}
 
 getSuffix :: String -> String
 getSuffix = last . splitOn "."
+{-# INLINE getSuffix #-}
 
-defaultMain :: Options -> IO ()
-defaultMain (Options inFl outFl pre th m svg dump gap gapMode avgMode) = do
-    let readMotif = case getSuffix inFl of
-                        "fasta" -> readFasta'
-                        _ -> readMEME
-        writeMotif = case getSuffix outFl of
-                        "fasta" -> writeFasta
-                        _ -> \fl x -> writeMEME fl x def
-    motifs <- readMotif inFl
-
-    if dump
-        then
-            mapM_ (\(a,b,c) -> B.putStrLn $ B.intercalate "\t" [a, b, B.pack $ show c]) $ pairDistance motifs
-        else do
-            let motifNumber = length motifs
-
-            hPutStrLn stderr $ printf "Merging Mode: %s" m
-            hPutStrLn stderr $ printf "Read %d motifs" motifNumber
-
-            motifs' <- case m of
-                "tree" -> do
-                    let (newMotifs, tree) = treeMerge th pre motifs gap avgFn
-                        fn x = B.unpack (_name x) ++ ": " ++ B.unpack (toBS $ toIUPAC $ _pwm x)
-
-                    case svg of
-                        Just fl -> do
-                            {-
-                            let w = 80
-                                h = 5 * fromIntegral motifNumber
-                            renderCairo fl (dims2D (10*w) (10*h)) $ drawDendrogram w h th tree fn ||| strutX 40
-                            -}
-                            return newMotifs
-                        _ -> return newMotifs
-                "iter" -> do
-                    let rs = iterativeMerge (alignmentBy jsd (pFn gap) avgFn) th motifs
-                    forM rs $ \(nm, pwm, ws) -> do
-                        let pwm' = dilute (pwm, ws)
-                        return $ Motif (B.intercalate "+" nm) pwm
-                 -- _ -> error "Unkown mode!"
-
-            hPutStrLn stderr $ printf "Write %d motifs" (length motifs')
-
-            writeMotif outFl motifs'
+mkAlignFn :: AlignOption -> AlignFn
+mkAlignFn (AlignOption gap gapMode avgMode) = alignmentBy jsd (pFn gap) avgFn
   where
     pFn = case gapMode of
         "linear" -> linPenal
@@ -163,6 +125,63 @@ defaultMain (Options inFl outFl pre th m svg dump gap gapMode avgMode) = do
         "l3" -> l3
         "max" -> lInf
         _ -> error "Unknown average mode"
+{-# INLINE mkAlignFn #-}
+
+readMotif :: FilePath -> IO [Motif]
+readMotif fl = case getSuffix fl of
+    "fasta" -> readFasta' fl
+    _ -> readMEME fl
+
+writeMotif :: FilePath -> [Motif] -> IO ()
+writeMotif fl motifs = case getSuffix fl of
+    "fasta" -> writeFasta fl motifs
+    _ -> writeMEME fl motifs def
+
+
+defaultMain :: CMD -> IO ()
+defaultMain (Dist a b alignOpt) = do
+    motifsA <- readMotif a
+    pairs <- case b of
+        Nothing -> return $ comb motifsA
+        Just b' -> do
+            motifsB <- readMotif b'
+            return [(x,y) | x <- motifsA, y <- motifsB]
+    forM_ pairs $ \(x,y) -> do
+        let (d,_) = alignFn (_pwm x) $ (_pwm y)
+        B.putStrLn $ B.intercalate "\t" [_name x, _name y, toShortest d]
+  where
+    alignFn = mkAlignFn alignOpt
+defaultMain (Merge inFl m th alignOpt outFl)= do
+    motifs <- readMotif inFl
+    let motifNumber = length motifs
+
+    hPutStrLn stderr $ printf "Merging Mode: %s" m
+    hPutStrLn stderr $ printf "Read %d motifs" motifNumber
+
+    motifs' <- case m of
+        "tree" -> do
+            let (newMotifs, tree) = treeMerge th "" motifs alignOpt
+                fn x = B.unpack (_name x) ++ ": " ++ B.unpack (toBS $ toIUPAC $ _pwm x)
+
+            {-
+            case svg of
+                Just fl -> do
+                    let w = 80
+                        h = 5 * fromIntegral motifNumber
+                    renderCairo fl (dims2D (10*w) (10*h)) $ drawDendrogram w h th tree fn ||| strutX 40
+                    return newMotifs
+                    -}
+            return newMotifs
+        "iter" -> do
+            let rs = iterativeMerge (mkAlignFn alignOpt) th motifs
+            forM rs $ \(nm, pwm, ws) -> do
+                let pwm' = dilute (pwm, ws)
+                return $ Motif (B.intercalate "+" nm) pwm
+         -- _ -> error "Unkown mode!"
+
+    hPutStrLn stderr $ printf "Write %d motifs" (length motifs')
+
+    writeMotif outFl motifs'
 {-# INLINE defaultMain #-}
 
 comb :: [a] -> [(a,a)]
@@ -175,5 +194,11 @@ main = execParser opts >>= defaultMain
   where
     opts = info (helper <*> parser)
             ( fullDesc
-           <> header (printf "Merge similar PWMs, version %s" v))
-    v = "0.2.0beta6" :: String
+           <> header (printf "Compare, align and merge motifs, version %s" v))
+    v = "0.2.0beta7" :: String
+    parser = subparser $ (
+        command "merge" (info (helper <*> mergeParser) $
+            fullDesc <> progDesc "Align and merge motifs")
+     <> command "dist" (info (helper <*> distParser) $
+            fullDesc <> progDesc "Align and compute pairwise distances")
+     )
