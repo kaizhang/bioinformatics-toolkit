417bdb629ce4d9a9a616a1c88f816aeff21abd7c
Kai Zhang <kai@kzhang.org>
2016-12-07 16:27:09 -0800

    add sortedBamToBedPE

--- src/Bio/Data/Bam.hs
+++ src/Bio/Data/Bam.hs
@@ -6,6 +6,7 @@ module Bio.Data.Bam
     , readBam
     , writeBam
     , bamToBed
+    , sortedBamToBedPE
     ) where
 
 import           Bio.Data.Bed
@@ -14,32 +15,46 @@ import           Bio.HTS.Types             (Bam, FileHeader (..))
 import           Conduit
 import           Control.Monad.State (get, lift)
 
+-- | Convert bam record to bed record. Unmapped reads will be discarded.
 bamToBed :: Conduit Bam HeaderState BED
-bamToBed = mapMC f =$= concatC
-  where
-    f bam = do
-        BamHeader hdr <- lift get
-        case getChr hdr bam of
-            Just chr ->
-                let start = fromIntegral $ position bam
-                    end = fromIntegral $ endPos bam
-                    nm = Just $ qName bam
-                    strand = Just $ not $ isRev bam
-                in return $ Just $ BED chr start end nm Nothing strand
-            _ -> return Nothing
+bamToBed = mapMC bamToBed1 =$= concatC
 {-# INLINE bamToBed #-}
 
-{-
-viewBam :: IdxHandle -> (B.ByteString, Int, Int) -> Source IO Bam1
-viewBam handle (chr, s, e) = case lookupTarget (idxHeader handle) chr of
-    Nothing -> return ()
-    Just chrId -> do
-        q <- lift $ query handle chrId (fromIntegral s,fromIntegral e)
-        go q
+-- | Convert pairedend bam file to bed. the bam file must be sorted by names,
+-- e.g., using "samtools sort -n". This condition is checked from Bam header.
+sortedBamToBedPE :: Conduit Bam HeaderState (BED, BED)
+sortedBamToBedPE = do
+    sortOrd <- getSortOrder <$> lift get
+    case sortOrd of
+        Queryname -> loopBedPE =$= concatC
+        _ -> error "Bam file must be sorted by NAME."
   where
-    go q' = do r <- lift $ next q'
-               case r of
-                   Nothing -> return ()
-                   Just bam -> yield bam >> go q'
-{-# INLINE viewBam #-}
--}
+    loopBedPE :: Conduit Bam HeaderState (Maybe (BED, BED))
+    loopBedPE = do
+        pair <- (,) <$$> await <***> await
+        case pair of
+            Nothing -> return ()
+            Just (bam1, bam2) -> if qName bam1 /= qName bam2
+                then error "Adjacent records have different query names. Aborted."
+                else do
+                    bed1 <- lift $ bamToBed1 bam1
+                    bed2 <- lift $ bamToBed1 bam2
+                    yield $ (,) <$> bed1 <*> bed2
+                    loopBedPE
+      where
+        (<$$>) = fmap . fmap
+        (<***>) = (<*>) . fmap (<*>)
+{-# INLINE sortedBamToBedPE #-}
+
+
+bamToBed1 :: Bam -> HeaderState (Maybe BED)
+bamToBed1 bam = do
+    BamHeader hdr <- lift get
+    return $ (\chr -> BED chr start end nm sc strand) <$> getChr hdr bam
+  where
+    start = fromIntegral $ position bam
+    end = fromIntegral $ endPos bam
+    nm = Just $ qName bam
+    strand = Just $ not $ isRev bam
+    sc = Just $ fromIntegral $ mapq bam
+{-# INLINE bamToBed1 #-}
