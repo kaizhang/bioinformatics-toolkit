0c03bf068ef7bb87d7b0ab66a4f0ed3066e59ede
Kai Zhang <kai@kzhang.org>
2017-03-08 15:08:36 -0800

    add motifScore

--- bioinformatics-toolkit-apps/app/motifScore.hs
+++ bioinformatics-toolkit-apps/app/motifScore.hs
@@ -9,37 +9,29 @@ import           Data.Semigroup                    ((<>))
 import           Data.Version                      (showVersion)
 import           Options.Applicative
 import           Paths_bioinformatics_toolkit_apps (version)
-import           System.IO                         (stdout)
-import           Text.Printf
-
+import Text.Printf
+import System.IO (stdout)
 
 data Options = Options
     { genomeFile :: FilePath
     , motifFile  :: FilePath
     , input      :: FilePath
-    , p          :: Double
     } deriving (Show, Read)
 
 parser :: Parser Options
 parser = Options
      <$> strArgument (metavar "GENOME")
      <*> strArgument (metavar "MOTIF_MEME")
-     <*> strArgument (metavar "INPUT")
-     <*> option auto
-           ( long "p-value"
-          <> short 'p'
-          <> value 1e-5
-          <> metavar "P-Value"
-          <> help "p-value cutoff. (default: 1e-5)" )
+     <*> strArgument
+        ( metavar "INPUT"
+       <> help "Motif binding sites in BED format, from running \"motifscan\".")
 
 defaultMain :: Options -> IO ()
 defaultMain opts = do
     withGenome (genomeFile opts) $ \genome -> do
         motifs <- readMEME $ motifFile opts
-        (readBed $ input opts :: Source IO BED3) =$=
-            motifScan genome motifs def (p opts) =$=
-            getMotifScore genome motifs def =$=
-            getMotifPValue Nothing motifs def $$ hWriteBed stdout
+        readBed (input opts) =$= getMotifScore genome motifs def $$
+            hWriteBed stdout
 
 main :: IO ()
 main = execParser opts >>= defaultMain
