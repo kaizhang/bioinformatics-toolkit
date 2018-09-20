06fa360dd5c2426fc6896b05f13c56920f8c6d64
Kai Zhang <kai@kzhang.org>
2018-09-20 11:17:13 -0700

    upgrade to lts-12.10

---
+++ bioinformatics-toolkit/src/Bio/RealWorld/Reactome.hs
@@ -0,0 +1,56 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
+
+module Bio.RealWorld.Reactome
+    ( getPathways
+    ) where
+
+import Data.Aeson
+import GHC.Generics (Generic)
+import           Conduit
+import qualified Data.Text as T
+import qualified Data.ByteString.Char8 as B
+import qualified Data.HashMap.Strict   as M
+import           Network.HTTP.Simple
+
+import           Bio.RealWorld.ID
+
+base :: String
+base = "https://reactome.org/ContentService"
+
+data Obj = Obj
+    { className	:: Maybe T.Text
+    , dbId :: Int 
+    , displayName :: T.Text
+    , schemaClass :: Maybe T.Text
+    , stId :: Maybe T.Text
+    , stIdVersion :: Maybe T.Text
+    } deriving (Show, Generic)
+
+instance ToJSON Obj
+instance FromJSON Obj
+ 
+-- | All Reactome top level pathways
+getPathways :: String -> IO [Obj]
+getPathways species = do
+    req <- parseRequest url
+    response <- httpJSON req
+    return $ getResponseBody response
+  where
+    url = base ++ "/data/pathways/top/" ++ species
+
+{-
+pathwayAnalysis :: [B.ByteString]   -- ^ A list of identifiers
+                -> IO B.ByteString
+pathwayAnalysis ids = do
+    initReq <- parseRequest base
+    let request = urlEncodedBody [] initReq
+    manager <- newManager tlsManagerSettings
+    r <- fmap M.fromList $ runResourceT $ do
+        response <- http request manager
+        runConduit $ responseBody response .| linesUnboundedAsciiC .|
+            (dropC 1 >> mapC ((\[a,b] -> (a,b)) . B.split '\t')) .| sinkList
+    return $ map (flip M.lookup r) ids
+  where
+    params =
+-}
